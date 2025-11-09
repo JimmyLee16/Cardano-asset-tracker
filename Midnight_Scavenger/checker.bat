8444baa4129caf86773e408ab9e64d4b4053c29f
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2025-11-09 18:50:35 +0700

    Update checker.bat

--- Midnight_Scavenger/checker.bat
+++ Midnight_Scavenger/checker.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -NoProfile -Command "Start-Process wt -ArgumentList 'powershell -NoProfile -ExecutionPolicy Bypass -File \"%~dp0Gui.ps1\"' -Verb RunAs"
+powershell -NoProfile -Command "Start-Process wt -ArgumentList 'powershell -NoProfile -ExecutionPolicy Bypass -File \"%~dp0Checksolution_gui.ps1\"' -Verb RunAs"
