17e8ee44e6c1703afd71594bf42d191a7997d082
JimmyLee16 <120309277+JimmyLee16@users.noreply.github.com>
2025-12-03 20:53:03 +0700

    Delete Midnight_Scavenger directory

--- Midnight_Scavenger/en/checker(en).bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Checksolution_gui(vie).ps1"
-pause
--- Midnight_Scavenger/vie/checker(vie).bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Force UTF-8 mode in console
-chcp 65001 >nul
-
-:: Run PowerShell with explicit UTF-8 encoding and bypass policy
-powershell -NoProfile -ExecutionPolicy Bypass ^
-    -Command "$OutputEncoding=[Console]::OutputEncoding=[Text.UTF8Encoding]::new($false); & '%~dp0Checksolution_gui(vie).ps1'"
-pause
